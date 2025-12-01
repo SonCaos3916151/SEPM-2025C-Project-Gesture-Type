7bfab1441bab5b338f87568b740dbb73e0c63d76
Son Cao <s3916151@rmit.edu.vn>
2025-12-02 01:49:37 +0700

    Add sign out button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@
 // import type { Schema } from "../amplify/data/resource";
 // import { generateClient } from "aws-amplify/data";
 import "./App.css"; // Ensure you keep your styles if needed
+import { useAuthenticator } from '@aws-amplify/ui-react';
+
+const { signOut } = useAuthenticator();
 
 function App() {
   return (
     <main>
       <h1>This page is for arcade mode</h1>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
