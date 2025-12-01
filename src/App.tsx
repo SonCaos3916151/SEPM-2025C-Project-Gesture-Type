5855dc66c919f6e611560a246e7b7924fb96a234
Son Cao <s3916151@rmit.edu.vn>
2025-12-02 01:30:31 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,12 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
   return (
     <main>
-      <h1>Arcade Mode page</h1>
+      <h1>This page is for arcade mode</h1>
     </main>
   );
 }
 
 export default App;
+
